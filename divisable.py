4e4bbc946748c6c9429d67a12a8d8defa351c380
Fenil <fdesai8268@gmail.com>
2025-07-02 23:16:12 +0530

    Add files via upload

---
+++ divisable.py
@@ -0,0 +1,11 @@
+def find_reminder(num,z):
+     return num%z
+   
+num=int(input("enter the num"))
+z=int(input("enter the Z"))
+
+if z==0:
+  print("no reminder")
+else:
+  reminder=find_reminder(num,z)
+  print(f"reminder when {num} divided by {z} then reminder is:",reminder)
