8d00cdfd69d8b3aea2e4da63245b772a18a049d6
Fenil <fdesai8268@gmail.com>
2025-07-02 23:15:51 +0530

    Add files via upload

---
+++ class and obj.py
@@ -0,0 +1,9 @@
+#class and object
+class person:
+    gender:"male"
+    def set(self,name,age):
+        self.name=Name
+        self.age=age
+    def display(self):
+        print(self.name,self.age)
+    
